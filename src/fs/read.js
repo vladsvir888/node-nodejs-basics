4996b637f5c880093a910e92a298bb81304db43e
vsvirydovich <vladsvir888@gmail.com>
2024-09-22 18:29:05 +0300

    feat: add src/fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readFile } from "fs/promises";
+import { errorText, checkPathExist } from "../helpers/index.js";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, "files/fileToRead.txt");
+
+  if (!(await checkPathExist(filePath))) {
+    throw new Error(errorText);
+  }
+
+  console.log(await readFile(filePath, { encoding: "utf8" }));
 };
 
 await read();
