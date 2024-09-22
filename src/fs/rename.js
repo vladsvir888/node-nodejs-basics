e13434a789dd83658d59562a4a8d39ec2f47665e
vsvirydovich <vladsvir888@gmail.com>
2024-09-22 17:13:12 +0300

    feat: add src/fs/rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { rename as fsRename } from "fs/promises";
+import { errorText, checkPathExist } from "../helpers/index.js";
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const oldFilePath = join(__dirname, "files/wrongFilename.txt");
+  const newFilePath = join(__dirname, "files/properFilename.md");
+
+  if (
+    !(await checkPathExist(oldFilePath)) ||
+    (await checkPathExist(newFilePath))
+  ) {
+    throw new Error(errorText);
+  }
+
+  await fsRename(oldFilePath, newFilePath);
 };
 
 await rename();
