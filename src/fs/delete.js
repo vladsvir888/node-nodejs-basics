43b543b7f48c6a17bbc2b879b673a4eb24a72c9f
vsvirydovich <vladsvir888@gmail.com>
2024-09-22 17:40:02 +0300

    feat: add src/fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { rm } from "fs/promises";
+import { errorText, checkPathExist } from "../helpers/index.js";
+
 const remove = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, "files/fileToRemove.txt");
+
+  if (!(await checkPathExist(filePath))) {
+    throw new Error(errorText);
+  }
+
+  rm(filePath);
 };
 
 await remove();
