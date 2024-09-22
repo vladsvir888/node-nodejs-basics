d41e4e2755f6c1d93e373278223188fa9666ab56
vsvirydovich <vladsvir888@gmail.com>
2024-09-22 14:36:45 +0300

    feat: add src/fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readdir, writeFile } from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filesPath = join(__dirname, "files");
+  const files = await readdir(filesPath);
+
+  if (files.includes("fresh.txt")) {
+    throw new Error("FS operation failed");
+  }
+
+  writeFile(join(filesPath, "fresh.txt"), "I am fresh and young");
 };
 
 await create();
