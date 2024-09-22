672ef5c82012a71b6d8d72adb7c62ff1b16ee0d0
vsvirydovich <vladsvir888@gmail.com>
2024-09-22 18:10:49 +0300

    feat: add src/fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readdir } from "fs/promises";
+import { errorText, checkPathExist } from "../helpers/index.js";
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filesPath = join(__dirname, "files");
+
+  if (!(await checkPathExist(filesPath))) {
+    throw new Error(errorText);
+  }
+
+  console.log(await readdir(filesPath));
 };
 
 await list();
