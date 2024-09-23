4649df88a02fe3c45f52123454926b39fb253d6d
vsvirydovich <vladsvir888@gmail.com>
2024-09-23 21:10:55 +0300

    feat: add src/cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const result = argv
+    .reduce((acc, current, index) => {
+      if (current.startsWith("--")) {
+        acc.push(`${current.slice(2)} is ${argv[index + 1]}`);
+      }
+      return acc;
+    }, [])
+    .join(", ");
+
+  console.log(result);
 };
 
 parseArgs();
