eaa25ca6f8bcc46d64019819d3e12121d82b9431
vsvirydovich <vladsvir888@gmail.com>
2024-09-24 23:12:37 +0300

    feat: add src/hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const file = join(__dirname, "files/fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+  const stream = createReadStream(file);
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log(`SHA256 hash as hex: ${hash.digest("hex")}`);
+  });
+
+  stream.on("error", (err) => {
+    console.log(`Error: ${err.message}`);
+  });
 };
 
 await calculateHash();
