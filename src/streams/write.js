9b3a7456b2968f1d32e14552c8c0141545f60103
vsvirydovich <vladsvir888@gmail.com>
2024-09-28 19:15:10 +0300

    feat: add src/streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createWriteStream } from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const file = join(__dirname, "files/fileToWrite.txt");
+  const stream = createWriteStream(file);
+
+  process.stdin.pipe(stream);
+
+  stream.on("error", (err) => {
+    console.log(`Error: ${err.message}`);
+  });
 };
 
 await write();
