9a6cb3b9b93c42c53e561d28b9de0283e681ed9a
vsvirydovich <vladsvir888@gmail.com>
2024-09-28 22:27:32 +0300

    feat: add src/streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const transformedData = chunk
+        .toString()
+        .trim()
+        .split("")
+        .reverse()
+        .join("");
+
+      this.push(transformedData);
+
+      callback();
+    },
+  });
+
+  process.stdin.pipe(reverseTransformStream).pipe(process.stdout);
 };
 
 await transform();
