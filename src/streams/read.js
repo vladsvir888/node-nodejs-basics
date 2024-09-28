2f5d56b847c4d836eb41f1a45478b3fb9eb50423
vsvirydovich <vladsvir888@gmail.com>
2024-09-28 15:17:56 +0300

    feat: add src/streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const file = join(__dirname, "files/fileToRead.txt");
+  const stream = createReadStream(file);
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on("error", (err) => {
+    console.log(`Error: ${err.message}`);
+  });
 };
 
 await read();
