a4f6a3610930a6c148e3dd395976be77288b0257
vsvirydovich <vladsvir888@gmail.com>
2024-10-03 19:34:40 +0300

    feat: add src/wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { Worker } from "worker_threads";
+import os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const cpuCores = os.cpus();
+  const start = 10;
+  const end = start + cpuCores.length;
+
+  const workers = [];
+
+  for (let i = start; i < end; i += 1) {
+    workers.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(join(__dirname, "worker.js"), {
+          workerData: i,
+        });
+
+        worker.on("message", (data) => resolve({ status: "resolved", data }));
+        worker.on("error", () => reject({ status: "error", data: null }));
+        worker.on("exit", (code) => {
+          if (code !== 0) {
+            reject(new Error(`Worker stopped with exit code ${code}`));
+          }
+        });
+      })
+    );
+  }
+
+  Promise.all(workers).then((response) => console.log(response));
 };
 
 await performCalculations();
