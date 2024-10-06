304857d01b3033865c49da2f909c379ad123ea91
vsvirydovich <vladsvir888@gmail.com>
2024-10-07 00:46:04 +0300

    fix: some rework src/streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,13 +1,21 @@
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import { createReadStream } from "fs";
+import { Transform } from "stream";
 
 const read = async () => {
   const __dirname = dirname(fileURLToPath(import.meta.url));
   const file = join(__dirname, "files/fileToRead.txt");
   const stream = createReadStream(file);
 
-  stream.pipe(process.stdout);
+  const newLineStream = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(`${chunk}\n`);
+      callback();
+    },
+  });
+
+  stream.pipe(newLineStream).pipe(process.stdout);
 
   stream.on("error", (err) => {
     console.log(`Error: ${err.message}`);
